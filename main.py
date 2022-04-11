270e0e1baabc7ab6767109383d3cd914b62c1c84
rizwaniqbalkhan92 <rizwaniqbalkhan92@gmail.com>
2022-04-11 23:53:15 +0500

    working on main

---
+++ main.py
@@ -0,0 +1,49 @@
+import mysql.connector as connector
+class DatabaseHelper:
+    def __init__(self):
+        self.con=connector.connect(host='localhost',port='3306',user='root',password='Rizwan42662@',database='sys')
+        query='create table if not exists user(user_id int primary key,user_name varchar(50),phone varchar(13))'
+        cur=self.con.cursor()
+        cur.execute(query)
+        print('Created Successfully...!!!');
+    def insertData(self,user_id,user_name,phone):
+        query='insert into user(user_id,user_name,phone) values({},"{}","{}")'.format(user_id,user_name,phone)
+        cursor=self.con.cursor()
+        cursor.execute(query)
+        self.con.commit()
+        print('Data save successfully....!!!');
+    def getAllData(self):
+        query="select * from user";
+        cursor=self.con.cursor();
+        cursor.execute(query);
+        self.con.commit()
+        for data in cursor:
+            print(data)
+    def getSingleData(self,user_id):
+        query="select * from user where user_id={}".format(user_id);
+        cursor=self.con.cursor();
+        cursor.execute(query);
+        self.con.commit()
+        for x in cursor:
+            print(x);
+    def deleteOne(self,user_id):
+        query="delete from user where user_id={}".format(user_id);
+        cursor=self.con.cursor();
+        cursor.execute(query);
+        self.con.commit()
+        print('Deleted Successfully...!!!');
+
+helper=DatabaseHelper();
+# helper.deleteOne(3);
+# helper.getSingleData(3)
+helper.getAllData();
+# helper.insertData(2,'rabeeb aqdas khan','03131057073');
+# helper.insertData(3,'abdul hai  khan','03131057074');
+# helper.insertData(4,'umar farooq','03131057076');
+# helper.insertData(5,'ali umair','03131057078');
+
+
+
+
+
+
